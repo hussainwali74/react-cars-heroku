809a5374223ea6ee6f80c1f89a5fb28e76bc7fec
unknown <13bscshwali@seecs.edu.pk>
2021-05-30 23:09:22 +0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import Cars from "./pages/Cars"
-
 function App() {
 	return (
 		<div className="min-h-screen px-4 py-6 font-sans text-black bg-gray-100 sm:px-2">
-			<Cars />
+			{/* <Cars /> */}
+			<h3>Hello world</h3>
 		</div>
 	)
 }
